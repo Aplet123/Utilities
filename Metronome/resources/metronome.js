6b2057490afeaf6fd8c77903c4916f28b6ef2f1a
Aplet123 <jasonqan2004@gmail.com>
2016-02-13 10:36:35 -0500

    Minor Changes 2
    Minor changes to the metronome.

--- Metronome/resources/metronome.js
+++ Metronome/resources/metronome.js
@@ -13,9 +13,9 @@ next.addEventListener("click",function(){if(Number(beat.value)<200){beat.value=N
 beat.addEventListener("mousemove",function(){beatCount.innerHTML=beat.value + " bpm";});
 back.addEventListener("click",function(){beatCount.innerHTML=beat.value + " bpm";});
 next.addEventListener("click",function(){beatCount.innerHTML=beat.value + " bpm";});
-beat.addEventListener("change",function(){if(start===true){window.clearInterval(window.interval);}});
-back.addEventListener("click",function(){if(start===true){window.clearInterval(window.interval);}});
-next.addEventListener("click",function(){if(start===true){window.clearInterval(window.interval);}});
+beat.addEventListener("change",function(){if(start===true){window.clearInterval(window.interval);display.innerHTML=1;}});
+back.addEventListener("click",function(){if(start===true){window.clearInterval(window.interval);display.innerHTML=1;}});
+next.addEventListener("click",function(){if(start===true){window.clearInterval(window.interval);display.innerHTML=1;}});
 time.addEventListener("keypress",function(){if((isNaN(Number(time.value))||Number(time.value)<=0)&&time.value!==""){time.value=1;}if(Number(time.value)%1!==0){time.value=Math.ceil(Number(time.value));}});
 var play = function(src,start,end)
 {
@@ -26,4 +26,4 @@ var play = function(src,start,end)
 	document.body.appendChild(audio);
 	window.setTimeout(function(){document.body.removeChild(audio);},(end-start)*1000);
 };
-display.addEventListener("click",function(){if(time.value===""){time.value=4;}if(start===false){start=true;metronome.className="metronome accent";play("http://www.freesfx.co.uk/rx2/mp3s/2/2449_1303610910.mp3",0.5,1);window.interval=window.setInterval(function(){if(Number(display.innerHTML)==time.value){metronome.className="metronome accent";play("http://www.freesfx.co.uk/rx2/mp3s/2/2449_1303610910.mp3",0.5,1);}else{metronome.className="metronome";play("http://www.freesfx.co.uk/rx2/mp3s/2/2449_1303610910.mp3",0,0.5);}display.innerHTML=Number(display.innerHTML)+1;if(display.innerHTML>Number(time.value)){display.innerHTML=1;}},60000/beat.value);}else{metronome.className="metronome";start=false;window.clearInterval(window.interval);}});
+display.addEventListener("click",function(){if(time.value===""){time.value=4;}if(start===false){start=true;metronome.className="metronome accent";play("http://www.freesfx.co.uk/rx2/mp3s/2/2449_1303610910.mp3",0.5,1);window.interval=window.setInterval(function(){if(Number(display.innerHTML)==time.value){metronome.className="metronome accent";play("http://www.freesfx.co.uk/rx2/mp3s/2/2449_1303610910.mp3",0.5,1);}else{metronome.className="metronome";play("http://www.freesfx.co.uk/rx2/mp3s/2/2449_1303610910.mp3",0,0.5);}display.innerHTML=Number(display.innerHTML)+1;if(display.innerHTML>Number(time.value)){display.innerHTML=1;}},60000/beat.value);}else{metronome.className="metronome";start=false;display.innerHTML=1;window.clearInterval(window.interval);}});
